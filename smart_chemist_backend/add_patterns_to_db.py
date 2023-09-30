1921370cdcc7d1c0ec95255bc97db32f0715c1a7
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-09-30 17:18:34 +0200

    Add lookup for ChEMBL, ChEBI, and PubChem

--- smart_chemist_backend/add_patterns_to_db.py
+++ smart_chemist_backend/add_patterns_to_db.py
@@ -1,7 +1,10 @@
 import django
 import pandas as pd
 import os
+from pathlib import Path
 
+HERE = Path(__file__).parent.resolve()
+SMARTS_HIERARCHY_PATH = HERE.parent.joinpath("smarts", "smarts_with_hierarchy.csv").resolve()
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smart_chemist_backend.settings')
 django.setup()
@@ -9,7 +12,7 @@ from smart_chemist.models import AnnotatedPattern
 
 AnnotatedPattern.objects.all().delete()
 
-data = pd.read_csv("../../smarts/smarts_with_hierarchy.csv", skiprows=1)
+data = pd.read_csv(SMARTS_HIERARCHY_PATH, skiprows=1)
 for index, row in data.iterrows():
     test = AnnotatedPattern.objects.create(smarts=row['SMARTS'],
                                            trivial_name=row['trivialname'],
