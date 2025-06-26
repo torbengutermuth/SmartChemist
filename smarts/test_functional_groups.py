f4af0b8d4f1fcab0a5cc7cb85f4fc1192dadcbb8
torbengutermuth <gutermuth@zbh.uni-hamburg.de>
2025-06-26 15:11:57 +0200

    Beta feedback revised
        - Went through all the feedback in the db again and addet testcases
          for it
        - Improved/added some patterns along the way
        - Improved the testing

--- smarts/test_functional_groups.py
+++ smarts/test_functional_groups.py
@@ -31,16 +31,28 @@ class FunctionGroupPatternTest(unittest.TestCase):
         current_pattern_to_test = None
         pattern_str = None
         for index,row in FunctionGroupPatternTest.df_molecules.iterrows():
+            currently_matching = False
             smiles, pattern, matching, comment, atomindices = row.values
             if pattern != current_pattern_to_test:
                 current_pattern_to_test = pattern
                 df = FunctionGroupPatternTest.functionals.query(f"trivialname == '{pattern}'")
-                self.assertEqual(df.shape[0], 1)
-                pattern_str = df.iloc[0]["SMARTS"]
-            if not pd.isna(atomindices):
-                self.assertTrue(check_expected_atom_matches(smiles, pattern_str, ast.literal_eval(atomindices)), msg=f"Problem with atomindex matching for the following case: {smiles, pattern_str, comment}")
-            else:
-                self.assertEqual(check_smiles_smarts_matching(smiles, pattern_str), matching, msg=f"Problem with patternmatching for the following case: {smiles, pattern_str, pattern , atomindices, comment}")
+                if df.shape[0] == 0:
+                    df = FunctionGroupPatternTest.biologicals.query(f"trivialname == '{pattern}'")
+                self.assertGreaterEqual(df.shape[0], 1)
+                pattern_str = [x for x in df["SMARTS"]]
+            for pattern in pattern_str:
+                if not pd.isna(atomindices):
+                    if check_expected_atom_matches(smiles, pattern, ast.literal_eval(atomindices)):
+                        currently_matching = True
+                else:
+                    if check_smiles_smarts_matching(smiles, pattern) == matching:
+                        currently_matching = True
+            if not pd.isna(atomindices) and not currently_matching:
+                print(f"Problem with atomindex matching for the following case: {smiles, pattern_str, comment}")
+            elif not currently_matching:
+                print(f"Problem with patternmatching for the following case: {smiles, pattern_str, pattern, atomindices, comment}")
+            self.assertTrue(currently_matching)
+
 
     def test_necessary_hierarchy(self):
         index_carbonyl = self.get_index_of_pattern("Carbonyl")
