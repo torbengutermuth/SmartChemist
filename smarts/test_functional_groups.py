107653c3e68001cbbaf2f522258c4b010cdf9cf4
gutermuth <gutermuth@zbh.uni-hamburg.de>
2025-06-26 07:55:39 +0200

    Hierarchy tests and more tests
        - Had a bug where acyl and aldehyde showed up simultaneously
        - Turns out due to missing new pattern database
        - Added some hierarchy tests to ensure this should never happen
    again on the server
        - Fixed an issue with the imine and iminum patterns

--- smarts/test_functional_groups.py
+++ smarts/test_functional_groups.py
@@ -12,6 +12,18 @@ class FunctionGroupPatternTest(unittest.TestCase):
     def setUpClass(cls):
         cls.df_patterns = pd.read_csv(Path(__file__).parent / "functional_groups.csv", skiprows=1)
         cls.df_molecules = pd.read_csv(Path(__file__).parent / "test_molecules_file.csv")
+        cls.df_all_patterns = pd.read_csv(Path(__file__).parent / "smarts_with_hierarchy.csv", skiprows=1)
+
+    def get_index_of_pattern(self,pattern_name):
+        list_of_indexes = self.df_all_patterns.index[self.df_all_patterns['trivialname'] == pattern_name].tolist()
+        self.assertEqual(len(list_of_indexes), 1)
+        return list_of_indexes[0]
+
+    def assure_hierarchy_exists(self, index_more_specific, index_less_specific):
+        row_of_interest = self.df_all_patterns.iloc[[index_more_specific]]
+        hierarchy_list = ast.literal_eval(row_of_interest['Hierarchy'].to_list()[0])
+        return int(index_less_specific) in hierarchy_list
+
 
     def test_molecule_file(self):
         current_pattern_to_test = None
@@ -28,6 +40,17 @@ class FunctionGroupPatternTest(unittest.TestCase):
             else:
                 self.assertEqual(check_smiles_smarts_matching(smiles, pattern_str), matching, msg=f"Problem with patternmatching for the following case: {smiles, pattern_str, pattern , atomindices, comment}")
 
+    def test_necessary_hierarchy(self):
+        index_carbonyl = self.get_index_of_pattern("Carbonyl")
+        index_acyl = self.get_index_of_pattern("Acyl group")
+        index_aldehyde = self.get_index_of_pattern("Aldehyde")
+        index_carboxyl = self.get_index_of_pattern("Carboxylic acid")
+        index_ketone = self.get_index_of_pattern("Ketone")
+        self.assertTrue(self.assure_hierarchy_exists(index_ketone, index_carbonyl))
+        self.assertTrue(self.assure_hierarchy_exists(index_carboxyl, index_carbonyl))
+        self.assertTrue(self.assure_hierarchy_exists(index_aldehyde, index_carbonyl))
+        self.assertTrue(self.assure_hierarchy_exists(index_ketone, index_acyl))
+        self.assertTrue(self.assure_hierarchy_exists(index_aldehyde, index_acyl))
 
 if __name__ == "__main__":
     unittest.main()
